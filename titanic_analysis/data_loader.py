c6075d0fe117b89d6e3b3657fafbd5d700cdc1a4
Abdul Samad <bds24lng@bangor.ac.uk>
2024-10-28 17:14:56 +0000

    third assignment updated

--- titanic_analysis/data_loader.py
+++ titanic_analysis/data_loader.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-def load_titanic_data(filepath: str) -> pd.DataFrame:
-    """
-    Loads the Titanic dataset from the specified file path.
-    
-    Args:
-        filepath (str): Path to the Titanic CSV file.
-    
-    Returns:
-        pd.DataFrame: Loaded Titanic dataset as a DataFrame.
-    """
-    pass  # Implement the loading logic here
+def get_numerical_df(df, numerical_features):
+    #filtering dataFrame
+    numerical_df = df[numerical_features].copy()
+    return numerical_df
+
+#test functions execution and output
+if _name_ == "_main_":
+    filepath = "data/titanic.csv" 
+    titanic_data = pd.read_csv(filepath)
+    numerical_features = ['Age', 'Fare']
+    numerical_df = get_numerical_df(titanic_data, numerical_features)
